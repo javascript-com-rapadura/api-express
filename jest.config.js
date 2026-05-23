dfe3c58c55d4763c839967a70dacbd5dfd32894c
João Arthur <tupi_1800@hotmail.com>
2026-05-22 21:53:19 -0300

    Melhora cobertura de teste

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: [
+    "app.js",
+    "services/**/*.js"
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
